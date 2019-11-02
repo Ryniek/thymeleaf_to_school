414373037d45a3386c4834c314e2b665bac79fe0
Michal Rynski <michalrynski96@gmail.com>
2019-11-02 13:24:58 +0100

    Initial commit

---
+++ src/main/java/pl/rynski/thymeleafschool/ThymeleafSchoolApplication.java
@@ -0,0 +1,13 @@
+package pl.rynski.thymeleafschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ThymeleafSchoolApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ThymeleafSchoolApplication.class, args);
+    }
+
+}
---
+++ src/test/java/pl/rynski/thymeleafschool/ThymeleafSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.rynski.thymeleafschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ThymeleafSchoolApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
